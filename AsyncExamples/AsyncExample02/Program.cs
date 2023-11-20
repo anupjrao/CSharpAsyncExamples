d18c613174b23fdc072c339de18846cd0b3db0e0
anupjrao <anup.j.rao.98@gmail.com>
2023-11-20 23:29:14 +0530

    Example02 - Simulating a long running task using Task.Delay()

---
+++ AsyncExamples/AsyncExample02/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace AsyncExample02
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Simulating some asynchronous process that takes some time to complete processing.
+        /// </summary>
+        public static async Task SimulateSomeLongProcess()
+        {
+            await Task.Delay(5000);
+            Console.WriteLine("Completed some long task");
+        }
+        public static async Task Main(string[] args)
+        {
+            await SimulateSomeLongProcess();
+        }
+    }
+}
