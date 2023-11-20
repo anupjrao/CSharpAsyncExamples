50dd9ae19ed54900f3596e89aeb641514ed0e850
anupjrao <anup.j.rao.98@gmail.com>
2023-11-20 23:18:30 +0530

    Initial commit - Example-01 - Basic async operation

---
+++ AsyncExamples/AsyncExample01/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AsyncExample01
+{
+    internal class Program
+    {
+        /// <summary>
+        /// This method simulates a request being made to a site asynchronously.
+        /// </summary>
+        /// <returns>The response from the request, stringified</returns>
+        public static async Task<string> GetDataAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync("https://google.com/");
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+        public static async Task Main(string[] args)
+        {
+            var result = await GetDataAsync();
+            Console.WriteLine(result);
+        }
+    }
+}
